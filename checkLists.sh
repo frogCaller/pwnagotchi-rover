2b835c937e5faa56b7a7da399d88a1ad3fc184c9
Tony <158815401+frogCaller@users.noreply.github.com>
2024-03-25 20:38:47 -0700

    Update checkLists.sh

--- checkLists.sh
+++ checkLists.sh
@@ -1,38 +1,15 @@
 #!/bin/bash
 
-# This script executes hashcat, using all text files within the specified "Lists" directory 
-# as potential password lists for the cracking attempt.
-# also, if user has cowsay or lolcat installed, then it will try to use them for nicer experience
+gen_candidates="sudo hcxpcapngtool /home/$USER/handshakes/*.pcap -o /home/$USER/handshakes/candidates.hc22000 -E /home/$USER/handshakes/essid.wordlist"
+check_lists="hashcat -m 22000 -w3 --status /home/$USER/handshakes/candidates.hc22000 /home/$USER/Lists/*.txt"
+check_cracks="hashcat -m 22000 /home/$USER/handshakes/candidates.hc22000 --show"
 
-current_user=$(whoami)
-check_lists="hashcat -m 22000 -w3 --status /home/$current_user/handshakes/candidates.hc22000 /home/$current_user/Lists/*.txt"
-check_cracks="hashcat -m 22000 candidates.hc22000 --show"
-
-# Function to check if a command exists
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-# Check if cowsay and lolcat are available
-if command_exists lolcat; then
-  echo "Checking with existing lists" | lolcat
-elif command_exists cowsay; then
-  echo "Checking with existing lists" | cowsay
-else
-  echo "Checking with existing lists"
-fi
-
+echo "Generating candidates"
+$gen_candidates
+wait $!
+echo "Checking existing lists"
 $check_lists
 wait $!
-
-# Check if cowsay and lolcat are available
-if command_exists lolcat; then
-  echo "I'm done checking" | lolcat
-elif command_exists cowsay; then
-  echo "I'm done checking" | cowsay
-else
-  echo "I'm done checking"
-fi
-
+echo "Done checking"
 $check_cracks
 wait $!
